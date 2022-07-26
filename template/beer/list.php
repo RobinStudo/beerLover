1d41bd259a8b5d0109dee148c44481239f908a1e
Robin Delbaere <hello@rdelbaere.fr>
2022-07-26 15:11:36 +0200

    Link to beer

--- template/beer/list.php
+++ template/beer/list.php
@@ -2,10 +2,10 @@
 
 <div class="card-container">
     <?php foreach ($data['beers'] as $beer) { ?>
-        <div class="card">
+        <a href="<?php echo $this->router->buildLink('beerSingle') . '?id=' . $beer->getId(); ?>" class="card">
             <img src="<?php echo $beer->getPicture(); ?>" alt="<?php echo $beer->getName(); ?>">
             <h3><?php echo $beer->getName(); ?></h3>
             <p><?php echo $beer->getStyle()->getName(); ?></p>
-        </div>
+        </a>
     <?php } ?>
 </div>
